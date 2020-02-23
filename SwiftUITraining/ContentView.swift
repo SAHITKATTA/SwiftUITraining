8e9d7a225cb842195790e46681212d8419acecf8
Sahit Katta <sahit.mohan@gmail.com>
2020-02-23 19:25:02 +0530

    Initial Commit

---
+++ SwiftUITraining/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITraining
+//
+//  Created by Sahit Katta on 23/02/20.
+//  Copyright © 2020 Sanjay Katta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
